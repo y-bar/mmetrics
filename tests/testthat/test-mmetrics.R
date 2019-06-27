ce73549f8ae1834d414d05f8fe17a3b8d4bcfae8
Nagi Teramo <teramonagi@gmail.com>
2019-06-27 19:46:26 +0900

    Add tests (#15)

--- tests/testthat/test-mmetrics.R
+++ tests/testthat/test-mmetrics.R
@@ -36,6 +36,11 @@ test_that("summarize all", {
   expect_equal(add(df, metrics = metrics), df_expected)
 })
 
+test_that("summarize all with summarize = FALSE", {
+  df_expected <- dplyr::mutate(df, cost=cost, ctr=click/impression)
+  expect_warning(expect_equal(add(df, metrics = metrics, summarize = FALSE), df_expected))
+})
+
 test_that("disaggregate for quosure (x/y)", {
   df_tested <- as.data.frame(dplyr::mutate(df, !!disaggregate(metrics[["ctr"]])))
   df_expected <- as.data.frame(dplyr::mutate(df, ctr=click/impression))
@@ -50,9 +55,16 @@ test_that("disaggregate for quosure (x)", {
   expect_true(all.equal(df_expected, df_tested, check.names = FALSE))
 })
 
+test_that("disaggregate for quosure (complicated)", {
+  expect_equal(rlang::quo_text(!!disaggregate(rlang::quo(sum(x)/sum(y) * aa))), "x/y * aa")
+})
+
 test_that("disaggregate for quosures", {
   df_tested <- dplyr::mutate(df, !!!disaggregate(metrics))
   df_expected <- dplyr::mutate(df, cost=cost, ctr=click/impression)
   expect_equal(df_tested, df_expected)
 })
 
+test_that("disaggregate should fail when arguemnt is not quosure(s)", {
+  expect_error(disaggregate("hoge"), "metrics must be quosure or quores")
+})
