dc6a2b62dbafab53083a5eaa027cdd2b7519c170
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2019-06-27 09:15:34 +0900

    Update

--- R/mmetrics.R
+++ R/mmetrics.R
@@ -20,20 +20,6 @@ define <- function(...){
   rlang::quos(...)
 }
 
-# Advertisng world metrics
-ad_metrics <- define(
-  cost = sum(cost),
-  impression = sum(impression),
-  click = sum(click),
-  conversion = sum(conversion),
-  ctr  = sum(click)/sum(impression),
-  cvr  = sum(conversion)/sum(click),
-  ctvr = sum(conversion)/sum(impression),
-  cpa  = sum(cost)/sum(conversion),
-  cpc  = sum(cost)/sum(click),
-  ecpm = sum(cost)/sum(impression) * 1000
-)
-
 #' Add metrics to data.frame
 #'
 #' Add metrics to data.frame
