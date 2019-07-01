8ecfad5f3ced2e3b64c20096c9090de8fcfd7ed9
Nagi Teramo <teramonagi@gmail.com>
2019-07-02 04:28:12 +0900

    Add data manual

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Dummy data.frame for this package
+#'
+#' A dataset contains columns often used in the digital marketing industry by user demographics
+#'
+#' @format A data frame with 10 rows and 6 variables:
+#' \describe{
+#'   \item{gender}{gender, Men (M) or Female (F)}
+#'   \item{age}{age}
+#'   \item{impression}{the number of how many times some ads are shown to users}
+#'   \item{click}{the number of how many times users click ads}
+#'   \item{conversion}{the number of how many times users converge}
+#' }
+"dummy_data"
